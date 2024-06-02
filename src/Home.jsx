23a72284eee2a782a06d2f29f385e40e3b615f82
unknown <panchi.romero345@gmail.com>
2024-06-01 22:04:11 -0400

    HomePage

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,46 @@
 import NavBar from "./components/navbar/NavBar.jsx"
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+
+import "./Home.css"
 
 function Home() {
+    
     return (
-        <>
-            <NavBar />
-            <div>
-                <h1>
-                    hola chavales
-                </h1>
+    
+    <div>
+        <NavBar />
+           
+    <section>  
+        <div>
+                <h1 className="Bienvenida">
+                   Bienvenido usuario
+                   
+            </h1>
+                
             </div>
-        </>
+        <div>
+            <h1 className="Eleccion">
+                Seleccione el area a trabajar
+            </h1>
+        </div>
+        
+        <div className="Botones">   
+            <Stack spacing={10} direction="row">
+                <Button variant="contained" className='GanaderiaButton' >GANADERIA</Button>
+                <Button variant="contained" className='AgriculturaButton' >AGRICULTURA</Button>
+            </Stack>
+         </div>
+         
+        <div>
+
+    </div>
+    
+    </section>
+    
+    </div>
     );
 }
 
