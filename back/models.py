dde81c4a6bbd778d0e206b3da6761e065fea58f5
Tomas Vainstub <vainstubtomas@gmail.com>
2024-07-03 15:42:11 -0300

    Clase gym

--- back/models.py
+++ back/models.py
@@ -14,3 +14,12 @@ class MainUser(models.Model):
 
     def __str__(self):
         return self.id
+
+
+class Gym(models.Model):
+    name = models.CharField(max_length=100, default="")
+    location = models.CharField(max_length=100, default="")
+    address = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return self.name
--- back/serializer.py
+++ back/serializer.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import MainUser
+from .models import MainUser, Gym
 
 
 class mainUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MainUser
+        fields = "__all__"
+
+
+class gymSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gym
         fields = "__all__"
