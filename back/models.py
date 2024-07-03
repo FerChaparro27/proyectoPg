1bcf4b36bf278995b1b187a5ee83901fdb65abdd
unknown <panchi.romero345@gmail.com>
2024-07-03 15:54:43 -0300

    .

--- back/models.py
+++ back/models.py
@@ -14,3 +14,24 @@ class MainUser(models.Model):
 
     def __str__(self):
         return self.id
+
+class Transactions(models.Model):
+    id = models.AutoField(primary_key=True)
+    numTransaction = models.TextField(max_length=100, default="")
+    dateTransaction = models.DateField()
+    amount = models.DecimalField()
+    origin = models.TextField()
+    addressee = models.TextField()
+    responsible = models.TextField()
+
+    def __str__(self):
+        return self.id
+
+class Vouchers(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.TextField()
+    detail = models.TextField()
+
+    def __str__(self):
+        return self.id
+    
--- back/serializer.py
+++ back/serializer.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import MainUser
+from .models import MainUser, Transactions, Vouchers
 
 
 class mainUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MainUser
+        fields = "__all__"
+
+class TransactionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transactions
         fields = "__all__"
