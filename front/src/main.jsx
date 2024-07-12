9c3bebf73f4108d91da04a2b4f5cecc2f7db5387
Tomas Vainstub <vainstubtomas@gmail.com>
2024-07-12 19:09:44 -0300

    Se creo la carpeta page_clients y se asigno el ruteo completo dentro del archivo app

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,38 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './HomePage/Home'
-import SignIn from './Signin/SignIn'
-import RecoverPassword from './RecoverPassword/RecoverPassword'
-import Register from './Register/Register'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './app';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { CssBaseline } from '@mui/material';
 
-
-function Main() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path='/recover-password' element={<RecoverPassword />} />
-        <Route path="/home" element={<Home />} />
-        <Route path='/register' element={<Register />}/>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline />
-    <Main />
-  </React.StrictMode>
-);
-
-
+    <CssBaseline/>
+    <App />
+  </React.StrictMode>,
+)
